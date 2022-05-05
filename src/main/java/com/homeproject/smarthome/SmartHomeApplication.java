07ef478078290a93e1dbe990c68421000aab1dc2
hungenin <genin83@gmail.com>
2022-05-06 01:16:49 +0200

    Add bean for rest template

--- src/main/java/com/homeproject/smarthome/SmartHomeApplication.java
+++ src/main/java/com/homeproject/smarthome/SmartHomeApplication.java
@@ -2,12 +2,21 @@ package com.homeproject.smarthome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class SmartHomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartHomeApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }

af88c92ec639256d0f4ac23a01e85ecb445141ed
hungenin <genin83@gmail.com>
2022-05-05 20:53:00 +0200

    Remove unnecessary functions

--- src/main/java/com/homeproject/smarthome/tvguide/service/ChannelService.java
+++ src/main/java/com/homeproject/smarthome/tvguide/service/ChannelService.java
@@ -1,10 +1,10 @@
-package com.homeproject.smarthome.tvGuide.service;
+package com.homeproject.smarthome.tvguide.service;
 
-import com.homeproject.smarthome.tvGuide.dao.ChannelDao;
-import com.homeproject.smarthome.tvGuide.exception.CannotBeDeletedException;
-import com.homeproject.smarthome.tvGuide.exception.DataNotFoundException;
-import com.homeproject.smarthome.tvGuide.model.Channel;
-import com.homeproject.smarthome.tvGuide.model.dto.ChannelDto;
+import com.homeproject.smarthome.tvguide.dao.ChannelDao;
+import com.homeproject.smarthome.tvguide.datagrabber.DataGrabber;
+import com.homeproject.smarthome.tvguide.exception.DataNotFoundException;
+import com.homeproject.smarthome.tvguide.model.Channel;
+import com.homeproject.smarthome.tvguide.model.dto.ChannelDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -14,47 +14,44 @@ import java.util.stream.Collectors;
 public class ChannelService {
     @Autowired
     private ChannelDao channelDao;
-
-    public ChannelDto add(Channel channel) {
-        channel.setId(null);
-        return new ChannelDto(channelDao.save(channel));
-    }
+    @Autowired
+    private DataGrabber dataGrabber;
 
     public ChannelDto get(Long id) {
         return new ChannelDto(channelDao.findById(id).orElseThrow(DataNotFoundException::new));
     }
 
-    public ChannelDto update(Long id, Channel channel) {
-        if (channelDao.existsById(id)) {
-            channel.setId(id);
+    public ChannelDto update(Channel channel) {
+        if (channelDao.existsById(channel.getId())) {
             return new ChannelDto(channelDao.save(channel));
         } else {
             throw new DataNotFoundException();
         }
     }
 
-    public void delete(Long id) {
-        Channel channel = channelDao.findById(id).orElseThrow(DataNotFoundException::new);
-
-        if (channel.getPrograms().isEmpty()) {
-            channelDao.deleteById(id);
-        } else {
-            throw new CannotBeDeletedException();
-        }
-    }
-
     public List<ChannelDto> channels() {
-        return channelDao.findAll().stream().map(ChannelDto::new).collect(Collectors.toList());
+        List<Channel> channels = channelDao.findAll();
+
+        if (channels.isEmpty()) {
+            dataGrabber.updateChannelList();
+            channels = channelDao.findAll();
+        }
+
+        return channels.stream().map(ChannelDto::new).collect(Collectors.toList());
     }
 
     public List<ChannelDto> followedChannels() {
         return channelDao.findChannelsByFollowEquals(true).stream().map(ChannelDto::new).collect(Collectors.toList());
     }
 
-    public void setFollow(Long id) {
-        channelDao.setFollow(id, true);
+    public void setAllFollow() {
+        channelDao.setAllFollow(true);
     }
-    public void setUnFollow(Long id) {
-        channelDao.setFollow(id, false);
+    public void setAllUnFollow() {
+        channelDao.setAllFollow(false);
+    }
+
+    public void updateChannelList() {
+        dataGrabber.updateChannelList();
     }
 }

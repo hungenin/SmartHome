a3a8d388f2991924d92b024be83e14296c55c4f9
Csanádi Balázs <genin83@gmail.com>
2022-07-23 00:25:53 +0200

    Delete unnecessary methods.

--- backend/src/main/java/com/homeproject/smarthome/tvguide/controller/api/ProgramApiController.java
+++ backend/src/main/java/com/homeproject/smarthome/tvguide/controller/api/ProgramApiController.java
@@ -1,19 +1,12 @@
 package com.homeproject.smarthome.tvguide.controller.api;
 
-import com.homeproject.smarthome.tvguide.exception.CannotBeAddedException;
 import com.homeproject.smarthome.tvguide.exception.DataNotFoundException;
-import com.homeproject.smarthome.tvguide.exception.InvalidDataException;
-import com.homeproject.smarthome.tvguide.model.Program;
 import com.homeproject.smarthome.tvguide.service.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 
-import static com.homeproject.smarthome.tvguide.response.HttpResponse.*;
-
-// TODO: kivételkezelést átnézni, helyre rakni
+import static com.homeproject.smarthome.tvguide.response.HttpResponse.dataNotFoundByIdResponse;
 
 @RestController
 @RequestMapping("/api/tv_guide/programs")
@@ -31,31 +24,8 @@ public class ProgramApiController {
         }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Program program, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            return invalidDataResponse(bindingResult);
-        }
-        try {
-            return ResponseEntity.ok(programService.update(id, program));
-        } catch (InvalidDataException e) {
-            return invalidDataResponse(e.getMessage());
-        } catch (CannotBeAddedException e) {
-            return dataNotFoundByIdResponse(e.getMessage());
-        } catch (DataNotFoundException e) {
-            return dataNotFoundByIdResponse("Program", id);
-        }
-    }
-
     @GetMapping
     public ResponseEntity<?> programs() {
         return ResponseEntity.ok(programService.programs());
-    }
-
-    @PostMapping("/refresh")
-    public ResponseEntity<?> refreshPrograms() {
-        programService.refreshPrograms();
-
-        return ResponseEntity.ok().build();
     }
 }
--- backend/src/main/java/com/homeproject/smarthome/tvguide/service/ProgramService.java
+++ backend/src/main/java/com/homeproject/smarthome/tvguide/service/ProgramService.java
@@ -1,15 +1,7 @@
 package com.homeproject.smarthome.tvguide.service;
 
-import com.homeproject.smarthome.tvguide.dao.ChannelDao;
-import com.homeproject.smarthome.tvguide.dao.ContentDao;
 import com.homeproject.smarthome.tvguide.dao.ProgramDao;
-import com.homeproject.smarthome.tvguide.datagrabber.DataGrabber;
-import com.homeproject.smarthome.tvguide.exception.CannotBeAddedException;
 import com.homeproject.smarthome.tvguide.exception.DataNotFoundException;
-import com.homeproject.smarthome.tvguide.exception.InvalidDataException;
-import com.homeproject.smarthome.tvguide.model.Channel;
-import com.homeproject.smarthome.tvguide.model.Content;
-import com.homeproject.smarthome.tvguide.model.Program;
 import com.homeproject.smarthome.tvguide.model.dto.ProgramDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,43 +12,12 @@ import java.util.stream.Collectors;
 public class ProgramService {
     @Autowired
     private ProgramDao programDao;
-    @Autowired
-    private ContentDao contentDao;
-    @Autowired
-    private ChannelDao channelDao;
-    @Autowired
-    private DataGrabber dataGrabber;
 
     public ProgramDto get(Long id) {
         return new ProgramDto(programDao.findById(id).orElseThrow(DataNotFoundException::new));
     }
 
-    public ProgramDto update(Long id, Program program) {
-        if (!program.getStart().isBefore(program.getEnd())) {
-            throw new InvalidDataException("{\"object\" : \"Program\",\"field\" : \"end\",\"rejected value\" : \"" + program.getEnd().toString() + "\",\"message\" : \"Start time must be before end time!\"}");
-        }
-
-        Content content = contentDao.findById(program.getContent().getId()).orElseThrow(() -> new CannotBeAddedException("{\"object\" : \"Program\",\"field\" : \"content\",\"rejected value\" : \"" + program.getContent().getId() + "\",\"message\" : \"Content not found!\"}"));
-        program.setContent(content);
-        if (!content.getPrograms().contains(program)) content.getPrograms().add(program);
-
-        Channel channel = channelDao.findById(program.getChannel().getId()).orElseThrow(() -> new CannotBeAddedException("{\"object\" : \"Program\",\"field\" : \"channel\",\"rejected value\" : \"" + program.getChannel().getId() + "\",\"message\" : \"Channel not found!\"}"));
-        program.setChannel(channel);
-        if (!channel.getPrograms().contains(program)) channel.getPrograms().add(program);
-
-        if (programDao.existsById(id)) {
-            program.setId(id);
-            return new ProgramDto(programDao.save(program));
-        } else {
-            throw new DataNotFoundException();
-        }
-    }
-
     public List<ProgramDto> programs() {
         return programDao.findAll().stream().map(ProgramDto::new).collect(Collectors.toList());
-    }
-
-    public void refreshPrograms() {
-        dataGrabber.refreshChannels();
     }
 }

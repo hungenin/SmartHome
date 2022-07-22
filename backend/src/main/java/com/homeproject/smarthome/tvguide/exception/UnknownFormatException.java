3ba283803e576efc8f91ddeade71488753d11afc
Csanádi Balázs <genin83@gmail.com>
2022-07-23 00:12:15 +0200

    Add unknokwn format exception.

---
+++ backend/src/main/java/com/homeproject/smarthome/tvguide/exception/UnknownFormatException.java
@@ -0,0 +1,11 @@
+package com.homeproject.smarthome.tvguide.exception;
+
+public class UnknownFormatException extends RuntimeException {
+    public UnknownFormatException() {
+        super();
+    }
+
+    public UnknownFormatException(String message) {
+        super(message);
+    }
+}

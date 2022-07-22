cb3143d443329eac8de021959184583746030ac3
Csanádi Balázs <genin83@gmail.com>
2022-07-23 00:32:41 +0200

    Delete unnecessary methods.

--- backend/src/main/java/com/homeproject/smarthome/tvguide/controller/api/ContentApiController.java
+++ backend/src/main/java/com/homeproject/smarthome/tvguide/controller/api/ContentApiController.java
@@ -1,17 +1,12 @@
 package com.homeproject.smarthome.tvguide.controller.api;
 
+import static com.homeproject.smarthome.tvguide.response.HttpResponse.dataNotFoundByIdResponse;
 import com.homeproject.smarthome.tvguide.exception.DataNotFoundException;
-import com.homeproject.smarthome.tvguide.model.Content;
 import com.homeproject.smarthome.tvguide.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 
-import static com.homeproject.smarthome.tvguide.response.HttpResponse.*;
-
-// TODO: kivételkezelést átnézni, helyre rakni
 
 @RestController
 @RequestMapping("/api/tv_guide/contents")
@@ -24,18 +19,6 @@ public class ContentApiController {
     public ResponseEntity<?> get(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(contentService.get(id));
-        } catch (DataNotFoundException e) {
-            return dataNotFoundByIdResponse("Content", id);
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Content content, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            return invalidDataResponse(bindingResult);
-        }
-        try {
-            return ResponseEntity.ok(contentService.update(id, content));
         } catch (DataNotFoundException e) {
             return dataNotFoundByIdResponse("Content", id);
         }
--- backend/src/main/java/com/homeproject/smarthome/tvguide/service/ContentService.java
+++ backend/src/main/java/com/homeproject/smarthome/tvguide/service/ContentService.java
@@ -2,7 +2,6 @@ package com.homeproject.smarthome.tvguide.service;
 
 import com.homeproject.smarthome.tvguide.dao.ContentDao;
 import com.homeproject.smarthome.tvguide.exception.DataNotFoundException;
-import com.homeproject.smarthome.tvguide.model.Content;
 import com.homeproject.smarthome.tvguide.model.dto.ContentDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,15 +15,6 @@ public class ContentService {
 
     public ContentDto get(Long id) {
         return new ContentDto(contentDao.findById(id).orElseThrow(DataNotFoundException::new));
-    }
-
-    public ContentDto update(Long id, Content content) {
-        if (contentDao.existsById(id)) {
-            content.setId(id);
-            return new ContentDto(contentDao.save(content));
-        } else {
-            throw new DataNotFoundException();
-        }
     }
 
     public List<ContentDto> contents() {
